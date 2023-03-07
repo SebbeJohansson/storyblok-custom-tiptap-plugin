61fe1d3b41e4bdf42f17f23fc6049e00cddcd40c
sebbejohansson <sebbe.johansson1995@gmail.com>
2023-03-07 18:58:04 +0100

    Attempts to add TipTap.

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,38 +1,41 @@
 <template>
   <div>
-    Google snippet preview:
-    <div class="p-metatags__preview">
-      <div class="p-metatags__google-title">{{ model.title || 'Your title' }}</div>
-      <div class="p-metatags__google-link">yoursite.com/example</div>
-      <div class="p-metatags__google-description">{{ model.description || 'Your description' }}</div>
-    </div>
-    <div class="uk-form-row">
-      <label>Meta Title</label>
-      <input type="text" placeholder="Your title" v-model="model.title" class="uk-width-1-1">
-    </div>
-
-    <div class="uk-form-row">
-      <label>Meta description</label>
-      <textarea rows="4" placeholder="Your description" v-model="model.description" class="uk-width-1-1"></textarea>
-    </div>
+    <editor-content :editor="model.editor" />
+    <h1>text</h1>
   </div>
 </template>
 
 <script>
+import { Editor, EditorContent } from '@tiptap/vue-2';
+import StarterKit from '@tiptap/starter-kit'
 export default {
+  components: {
+    EditorContent,
+  },
   mixins: [window.Storyblok.plugin],
+  template: `
+    <div>
+      <editor-content :editor="model.editor" />
+      <input class="uk-width-1-1" v-model="model.example" />
+    </div>
+  `,
   methods: {
     initWith() {
+      console.log('plugin:initWith');
+      const editor = new Editor({
+        content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+        extensions: [
+          StarterKit,
+        ],
+      });
       return {
-        // needs to be equal to your storyblok plugin name
-        plugin: 'my-plugin-name',
-        title: '',
-        description: ''
-      }
+        editor,
+        plugin: 'tiptap-test',
+        example: 'Hello World!'
+      };
     },
     pluginCreated() {
-      // eslint-disable-next-line
-      console.log('View source and customize: https://github.com/storyblok/storyblok-fieldtype')
+      console.log('plugin:created');
     }
   },
   watch: {
@@ -43,23 +46,5 @@ export default {
       deep: true
     }
   }
-}
+};
 </script>
-
-<style>
-  .p-metatags__google-title {
-    color: blue;
-    text-decoration: underline;
-  }
-
-  .p-metatags__google-link {
-    color: green;
-  }
-
-  .p-metatags__preview {
-    margin: 5px 0 15px;
-    padding: 10px;
-    color: #000;
-    background: #FFF;
-  }
-</style>
