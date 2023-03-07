70f2285b0e1f5afc471b97f4a49c40579d3d9595
sebbejohansson <sebbe.johansson1995@gmail.com>
2023-03-07 22:21:34 +0100

    Adds an example with ckeditor that is working.

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,50 +1,82 @@
 <template>
   <div>
-    <editor-content :editor="model.editor" />
-    <h1>text</h1>
+    {{ model }}
+    <!-- {{  editorRandomVariable123578 }} -->
+    <!-- <editor-content :editor="model.editor" /> -->
+    <h1>text new</h1>
+    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
   </div>
 </template>
 
 <script>
-import { Editor, EditorContent } from '@tiptap/vue-2';
-import StarterKit from '@tiptap/starter-kit'
+// import { Editor, EditorContent } from '@tiptap/vue-2';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+// import StarterKit from '@tiptap/starter-kit'
 export default {
-  components: {
-    EditorContent,
-  },
+  // components: {
+  //   EditorContent,
+  // },
   mixins: [window.Storyblok.plugin],
-  template: `
-    <div>
-      <editor-content :editor="model.editor" />
-      <input class="uk-width-1-1" v-model="model.example" />
-    </div>
-  `,
+  // data() {
+  //   return {
+  //     editorRandomVariable123578: null,
+  //   }
+  // },
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorData: '<p>Content of the editor.</p>',
+      // editorConfig: {
+      //   // The configuration of the editor.
+      // }
+    };
+  },
   methods: {
     initWith() {
-      console.log('plugin:initWith');
-      const editor = new Editor({
-        content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
-        extensions: [
-          StarterKit,
-        ],
-      });
+      console.log('plugin:initWith 2');
       return {
-        editor,
         plugin: 'tiptap-test',
-        example: 'Hello World!'
+        example: 'Hello World!',
+        // editor: null,
       };
     },
     pluginCreated() {
+      // console.log("editor");
+      // const editor = new Editor({
+      //   content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+      // });
+      // this.model.editor = editor;
+      // console.log(editor);
+      // console.log(JSON.stringify(editor));
       console.log('plugin:created');
+      // this.editor = JSON.stringify(editor);
     }
   },
-  watch: {
-    'model': {
-      handler: function (value) {
-        this.$emit('changed-model', value);
-      },
-      deep: true
-    }
-  }
+  // mounted() {
+  //   console.log("mounted");
+  //   this.editorRandomVariable123578 = new Editor({
+  //     content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+  //     // extensions: [
+  //     //   StarterKit,
+  //     // ],
+  //     onUpdate: () => {
+  //       // HTML
+  //       // this.$emit('input', this.editor.getHTML())
+
+  //       // JSON
+  //       console.log(this.editor.getJSON());
+  //       this.$emit('input', this.editor.getJSON())
+  //     },
+  //   });
+  //   console.log(this.editorRandomVariable123578);
+  // },
+  // watch: {
+  //   'model': {
+  //     handler: function (value) {
+  //       this.$emit('changed-model', value);
+  //     },
+  //     deep: true
+  //   }
+  // }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
+import { /* Editor,  */EditorContent } from '@tiptap/vue-2';
+import CKEditor from '@ckeditor/ckeditor5-vue2';
 import Plugin from './Plugin.vue'
 
+// window.Storyblok.vue.use(Editor);
+window.Storyblok.vue.use(EditorContent);
+window.Storyblok.vue.use(CKEditor);
 if (process.env.NODE_ENV == 'development') {
 
   window.Fieldtype = Plugin
